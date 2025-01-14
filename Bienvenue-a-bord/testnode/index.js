89753bc8aa4c3765c0e88be814fff20219a1689a
alesbesdria <melissa@meirsman.fr>
2025-01-14 12:04:43 +0100

    new add install chalk gt ignore

--- Bienvenue-a-bord/testnode/index.js
+++ Bienvenue-a-bord/testnode/index.js
@@ -1,5 +1,3 @@
-const name = 'John Doe';
-const age = 26;
-const passions = ['Ski', 'Hiking', 'Cinema'];
-
-console.log(name, age, passions);
+const userInformation = require('./userInformation');
+console.log(`Hi, my name is ${userInformation.name} and I'm ${userInformation.age} years old`);
+userInformation.displayPassions();
