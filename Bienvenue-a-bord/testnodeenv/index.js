f8e66d27d1dc9278cfeba853c93565181c44c5d1
alesbesdria <melissa@meirsman.fr>
2025-01-15 10:20:25 +0100

    testnodejs

--- Bienvenue-a-bord/testnodeenv/index.js
+++ Bienvenue-a-bord/testnodeenv/index.js
@@ -1,1 +1,7 @@
-console.log('I am <name>, wilder in <city>, and I love <language></language>');
+import "dotenv/config";
+
+// const name = `${MY_NAME}`;
+// const city = `${MY_CITY}`;
+// const language = `${MY_LANGUAGE}`;
+
+console.log(`I am ${process.env.MY_NAME}, wilder in ${process.env.MY_CITY}, and I love ${process.env.MY_LANGUAGE}`);
